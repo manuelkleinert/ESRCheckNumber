12fb16c5fcf2ef780fe406dbe04c1ac7852d298b
Manuel Kleinert <info@manuelkleinert.ch>
2020-05-18 13:39:38 +0200

    Add function

---
+++ EsrCheckNumber.php
@@ -0,0 +1,16 @@
+<?php
+
+class EsrCheckNumber
+{
+    private $checkList = [0, 9, 4, 6, 8, 2, 7, 1, 3, 5];
+
+    private function getCheckNumber($referenceNumber)
+    {
+        $transferNumber = 0;
+        $referenceNumber = str_replace(' ', '', $referenceNumber);
+        foreach(str_split($referenceNumber) as $index => $number) {
+            $transferNumber = $this->checkList[($transferNumber + (int)$number) %10];
+        }
+        return (10 - $transferNumber) %10;
+    }
+}
